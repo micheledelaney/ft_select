7014ef24208c78320d372ca06bb17e4bb8a30cee
cmicheledelaney <c.michele.delaney@gmail.com>
2019-04-05 15:49:20 -0700

    no more newline when nthing gets returned

--- srcs/print_selected_files.c
+++ srcs/print_selected_files.c
@@ -2,17 +2,20 @@
 
 void	print_selected(t_files *files)
 {
-	int i;
+	int		i;
+	bool	printed;
 
 	i = -1;
+	printed = false;
 	while (files->files[++i])
 	{
 		if (files->selected[i] != -1)
 		{
 			ft_putstr_fd(files->files[i], STDOUT_FILENO);
 			ft_putstr_fd(" ", STDOUT_FILENO);
+			printed = true;
 		}
 	}
-	ft_putstr_fd("\n", STDOUT_FILENO);
+	(printed == true) ? (ft_putstr_fd("\n", STDOUT_FILENO)) : (0);
 	free(files->selected);
 }
